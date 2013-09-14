aadefd83a2446eb76975fcc1ab66311ae5715506
maq128 <maq128@126.com>
2013-09-14 22:47:53 +0800

    页面中检测插件版本，便于升级后提示用户。

--- src/content.js
+++ src/content.js
@@ -1,92 +1,94 @@
 // http://developer.chrome.com/extensions/devguide.html
 
-(function() {
-	// 确认“运行”按钮
-	var btn = document.getElementById( 'tae-js-testbed-run' );
-	if ( ! btn ) return;
+// 确认“运行”按钮
+var btn = document.getElementById( 'tae-js-testbed-run' );
+if ( ! btn ) return;
 
-	// 确认“运行结果”的 irame
-	var iframe = document.getElementById( 'tae-js-testbed-iframe' );
-	if ( ! iframe ) return;
+// 埋下插件版本标记
+var m = chrome.runtime.getManifest();
+btn.setAttribute( 'data-ext-version', m.version );
 
-	// 传值对象
-	var data = {};
+// 确认“运行结果”的 irame
+var iframe = document.getElementById( 'tae-js-testbed-iframe' );
+if ( ! iframe ) return;
 
-	// 点击了“运行”按钮
-	btn.addEventListener( 'click', function( evt ) {
-		// 提取 html 内容
-		var html = document.getElementById( 'tae-js-testbed-html' );
-		data.html = html ? html.value : '';
+// 传值对象
+var data = {};
 
-		// 提取 js 内容
-		var js = document.getElementById( 'tae-js-testbed-js' );
-		data.js = js ? js.value : '';
+// 点击了“运行”按钮
+btn.addEventListener( 'click', function( evt ) {
+	// 提取 html 内容
+	var html = document.getElementById( 'tae-js-testbed-html' );
+	data.html = html ? html.value : '';
 
-		// 提取 js 环境版本
-		var ver = document.getElementById( 'tae-js-testbed-ver' );
-		data.ver = ver ? ver.value : 'r3002';
+	// 提取 js 内容
+	var js = document.getElementById( 'tae-js-testbed-js' );
+	data.js = js ? js.value : '';
 
-		// 对于 r4000 环境，要从 html 中提取出 modules
-		if ( data.ver == 'r4000' ) {
-			// <cajamodules inlucde="kissy/1.3.0/core,kissy/gallery/kcharts/1.1/index"></cajamodules>
-			var m = data.html.match( /<cajamodules\s+inlucde=['"]([^'"]*)['"]/i );
-			if ( m ) {
-				// 每个 module 都加上 'openjs/' 前缀
-				var modules = m[1].split( ',' );
-				for ( var i in modules ) {
-					modules[i] = 'openjs/' + modules[i].trim();
-				}
-				data.modules = modules.join( ',' );
+	// 提取 js 环境版本
+	var ver = document.getElementById( 'tae-js-testbed-ver' );
+	data.ver = ver ? ver.value : 'r3002';
+
+	// 对于 r4000 环境，要从 html 中提取出 modules
+	if ( data.ver == 'r4000' ) {
+		// <cajamodules inlucde="kissy/1.3.0/core,kissy/gallery/kcharts/1.1/index"></cajamodules>
+		var m = data.html.match( /<cajamodules\s+inlucde=['"]([^'"]*)['"]/i );
+		if ( m ) {
+			// 每个 module 都加上 'openjs/' 前缀
+			var modules = m[1].split( ',' );
+			for ( var i in modules ) {
+				modules[i] = 'openjs/' + modules[i].trim();
 			}
+			data.modules = modules.join( ',' );
 		}
+	}
 
-		// 请求 caja 编译
-		var xhr = new XMLHttpRequest();
-		xhr.open( 'POST', 'http://zxn.taobao.com/tbcajaService.htm', true );
-		xhr.setRequestHeader( 'Content-type', 'application/x-www-form-urlencoded' );
-		xhr.onreadystatechange = function( evt ) {
-			if ( xhr.readyState != 4 ) return;
-			if ( xhr.status != 200 ) return;
+	// 请求 caja 编译
+	var xhr = new XMLHttpRequest();
+	xhr.open( 'POST', 'http://zxn.taobao.com/tbcajaService.htm', true );
+	xhr.setRequestHeader( 'Content-type', 'application/x-www-form-urlencoded' );
+	xhr.onreadystatechange = function( evt ) {
+		if ( xhr.readyState != 4 ) return;
+		if ( xhr.status != 200 ) return;
 
-			// caja 编译完成，对得到的结果进行一些清洗
-			var caja = xhr.responseText;
-			caja = caja.substr( caja.indexOf( 'TShop.' ) )
-					.replace( /&#39;/g, '\'' )
-					.replace( /&quot;/g, '"' )
-					.replace( /&amp;/g, '&' )
-					.replace( /\\\\"/g, '\\\"' );
-			data.caja = caja;
+		// caja 编译完成，对得到的结果进行一些清洗
+		var caja = xhr.responseText;
+		caja = caja.substr( caja.indexOf( 'TShop.' ) )
+				.replace( /&#39;/g, '\'' )
+				.replace( /&quot;/g, '"' )
+				.replace( /&amp;/g, '&' )
+				.replace( /\\\\"/g, '\\\"' );
+		data.caja = caja;
 
-			// 重新加载 iframe
-			var href = window.location.href;
-			var pos = href.lastIndexOf( '/' );
-			href = href.substr( 0, pos ) + '/bed.' + data.ver + '.html';
-			if ( data.modules ) {
-				// 通过 url 传入 modules
-				href += '?' + encodeURIComponent( data.modules );
-			}
-			iframe.contentWindow.location = href;
-		};
-		xhr.send( 'token=TAE-SDK&content=' + encodeURIComponent( data.js ) + '&component=tae_js_testbed' );
-	});
+		// 重新加载 iframe
+		var href = window.location.href;
+		var pos = href.lastIndexOf( '/' );
+		href = href.substr( 0, pos ) + '/bed.' + data.ver + '.html';
+		if ( data.modules ) {
+			// 通过 url 传入 modules
+			href += '?' + encodeURIComponent( data.modules );
+		}
+		iframe.contentWindow.location = href;
+	};
+	xhr.send( 'token=TAE-SDK&content=' + encodeURIComponent( data.js ) + '&component=tae_js_testbed' );
+});
 
-	// iframe 加载完毕
-	iframe.addEventListener( 'load', function( evt ) {
-		// 填入 html 内容
-		var doc = iframe.contentWindow.document;
-		var mod = doc.getElementById( 'tae-js-testbed-mod' );
-		mod.innerHTML = data.html;
+// iframe 加载完毕
+iframe.addEventListener( 'load', function( evt ) {
+	// 填入 html 内容
+	var doc = iframe.contentWindow.document;
+	var mod = doc.getElementById( 'tae-js-testbed-mod' );
+	mod.innerHTML = data.html;
 
-		// 引入 caja 编译后的 js 内容
-		var sc = doc.createElement( 'script' );
-		sc.innerHTML = data.caja;
-		doc.body.appendChild( sc );
+	// 引入 caja 编译后的 js 内容
+	var sc = doc.createElement( 'script' );
+	sc.innerHTML = data.caja;
+	doc.body.appendChild( sc );
 
-		// 启动沙箱环境
-		sc = doc.createElement( 'script' );
-		sc.src = data.ver == 'r4000'
-			? 'http://a.tbcdn.cn/apps/taesite/balcony/core/r4000/base/setup.js'
-			: 'http://a.tbcdn.cn/apps/??taesite/balcony/core/r3002/caja-setup.js,daogoudian/isv/kissygallery.js';
-		doc.body.appendChild( sc );
-	});
-})();
+	// 启动沙箱环境
+	sc = doc.createElement( 'script' );
+	sc.src = data.ver == 'r4000'
+		? 'http://a.tbcdn.cn/apps/taesite/balcony/core/r4000/base/setup.js'
+		: 'http://a.tbcdn.cn/apps/??taesite/balcony/core/r3002/caja-setup.js,daogoudian/isv/kissygallery.js';
+	doc.body.appendChild( sc );
+});
