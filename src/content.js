fd70db3a8ca262cb6b162699b64c038889f4cc5a
maq128 <maq128@126.com>
2013-09-15 23:40:40 +0800

    BUGFIX: 重复运行时没有清除残留的 cajamodules 痕迹。

--- src/content.js
+++ src/content.js
@@ -30,6 +30,7 @@
 		var ver = document.getElementById( 'tae-js-testbed-ver' );
 		data.ver = ver ? ver.value : 'r3002';
 
+		delete data.modules;
 		// 对于 r4000 环境，要从 html 中提取出 modules
 		if ( data.ver == 'r4000' ) {
 			// <cajamodules inlucde="kissy/1.3.0/core,kissy/gallery/kcharts/1.1/index"></cajamodules>
@@ -62,7 +63,6 @@
 			data.caja = caja;
 
 			// 重新加载 iframe
-			// FIXME: 似乎这里的“重新加载”方式并不能彻底清空上一次加载留下的痕迹……？
 			var href = window.location.href;
 			var pos = href.lastIndexOf( '/' );
 			href = href.substr( 0, pos ) + '/bed.' + data.ver + '.html';
